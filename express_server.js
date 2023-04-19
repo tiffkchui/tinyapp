8ef71d195862971cc69061ecb08e7c82b9636815
tiffkchui <124648372+tiffkchui@users.noreply.github.com>
2023-04-18 22:07:02 -0700

    login

--- express_server.js
+++ express_server.js
@@ -73,6 +73,12 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post('/login', (req, res) => {
+  const { username } = req.body;
+  res.cookie('username', username);
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
