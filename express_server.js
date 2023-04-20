28ef7d270f766a9fb1ab0c4c2fdc9969b926b763
tiffkchui <124648372+tiffkchui@users.noreply.github.com>
2023-04-19 18:13:00 -0700

    experimenting

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
@@ -73,10 +75,23 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  res.render("urls_index", templateVars);
+});
+
 app.post('/login', (req, res) => {
-  const { username } = req.body;
+  const username = req.cookies.username;
   res.cookie('username', username);
   res.redirect('/urls');
+});
+
+app.use((req, res, next) => {
+  res.locals.username = req.cookies.username;
+  next();
 });
 
 app.listen(PORT, () => {
